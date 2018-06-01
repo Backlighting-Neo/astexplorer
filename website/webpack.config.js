cd75aefc9ef24b23691c3fba26952af95f39faa0
tangzongchao <tangzongchao@meituan.com>
2018-06-01 14:41:58 +0800

    try to support thrift

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -168,6 +168,7 @@ module.exports = Object.assign({
           path.join(__dirname, 'node_modules', 'webidl2'),
           path.join(__dirname, 'node_modules', 'tslint'),
           path.join(__dirname, 'node_modules', 'tslib'),
+          path.join(__dirname, 'node_modules', '@creditkarma', 'thrift-parser', 'dist'),
           path.join(__dirname, 'src'),
         ],
         loader: 'babel-loader',
